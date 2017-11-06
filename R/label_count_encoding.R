a297224994bda310cec6fa72686215890db1bb7f
Germayne <germayneng@hotmail.com>
2017-11-06 16:57:30 +0800

    Update label_count_encoding.R

--- R/label_count_encoding.R
+++ R/label_count_encoding.R
@@ -11,17 +11,20 @@
 #' @export
 
 
-label_count_encoding <- function(id){
 
+label_count_encoding <- function(id){
+  
   working_df <- data.table::data.table(name = id)
   working_df$name <- as.factor(working_df$name)
   summary_count <- table(id) %>% data.frame()
   colnames(summary_count) <- c("name","count")
   summary_count$name <- as.factor(summary_count$name)
+  
+  
+  summary_count <- summary_count %>% arrange(count) %>%  mutate(encoded = row_number()) 
   working_df <- working_df %>% left_join(summary_count, by = "name")
-  working_df <- working_df %>% arrange(desc(count)) %>% group_by(name) %>%
-    mutate(value = as.numeric(name)) %>% ungroup() %>%
-    group_by(name) %>% mutate(encoded = (n_distinct(working_df$name) +1) - value) %>% ungroup
-  return(working_df$encoded)
+  
 
+  return(working_df$encoded)
+  
 }
